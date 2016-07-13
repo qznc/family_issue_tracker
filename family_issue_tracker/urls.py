452406af4ec3e5556249903302b9664e4a61ff91
Andreas Zwinkau <zwinkau@kit.edu>
2016-07-13 14:39:41 +0200

    Redirect base url

--- family_issue_tracker/urls.py
+++ family_issue_tracker/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^i/', include('issues.urls')),
+    url(r'^$', RedirectView.as_view(url="i/")),
 ]
